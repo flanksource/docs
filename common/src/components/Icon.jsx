455300111ab4c48328ae569cb445b1d5c8caa678
Moshe Immermam <moshe@flanksource.com>
2024-07-14 23:38:00 +0300

    chore: doc updates

--- common/src/components/Icon.jsx
+++ common/src/components/Icon.jsx
@@ -1,13 +1,28 @@
+
 import React from "react";
-export default function Icon({ name, height = "32px", className, url }) {
-  let img = <img
-    src={`/img/icons/${name}.svg`}
-    style={{ height: { height } }}
-    className={className}
-  />;
+import { IconMap as Icons } from "@flanksource/icons/mi";
+import clsx from "clsx"
+
+export default function Icon({ name, height = 22, className, url, children }) {
+  name = name
+    .replaceAll("--", "-")
+    .replaceAll("::", "-")
+    .toLowerCase()
+    .replaceAll("k8-", "k8s-")
+    .replaceAll("kubernetes-", "k8s-");
+
+  let IconSVG = Icons[name];
+
+  if (!IconSVG) {
+    return <span>{name}</span>
+  }
+  let img = <span className={clsx("align-middle", className)}><IconSVG className="my-auto" name={name} size={height} />
+    <span className="pl-1.5">{children}</span>
+  </span>
 
   if (url != null) {
-    return <a href={url} className="pr-2">{img}</a>;
+    return <a href={url}>{img}</a>
   }
   return img
 }
+
