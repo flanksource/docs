775e09f1e0469b42d0640308874164d24d859f5f
Moshe Immermam <moshe@flanksource.com>
2024-12-16 15:15:08 +0200

    chore: doc fixes

--- common/src/components/ShadowBox.jsx
+++ common/src/components/ShadowBox.jsx
@@ -1,13 +1,14 @@
-
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Screenshot({ img, size, shadow = true, alt }) {
   if (shadow === true) {
     return <div className='rounded-8px mb-3  shadow-card card bg-lightest-gray border-0 border-t-8 border-white ml-auto mr-auto' style={{ width: size }}>
-      <img src={img} width={size} />
+      <img src={require(`@site/static/${img}`).default} width={size} />
     </div>
   } else {
     return <div className='my-3  ml-auto mr-auto' style={{ width: size, flexDirection: "column", display: "flex" }}>
-      <img src={img} width={size} />
+
+      <img src={require(`@site/static${img}`).default} width={size} />
       <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">{alt}</figcaption>
 
     </div>
