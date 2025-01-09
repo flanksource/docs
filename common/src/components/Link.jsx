3712f296eabf5057f3d0b81f843298bb1a2d1d0a
Moshe Immermam <moshe@flanksource.com>
2025-01-09 17:25:12 +0200

    chore: baseLink fixes

--- common/src/components/Link.jsx
+++ common/src/components/Link.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Link from '@docusaurus/Link';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export function CommonLink({ children, to, anchor }) {
   const { siteConfig } = useDocusaurusContext();
-  let url = siteConfig.customFields.links[to]
+  let url = useBaseUrl(siteConfig.customFields.links[to])
   if (anchor != null) {
     url += "#" + anchor
   }
