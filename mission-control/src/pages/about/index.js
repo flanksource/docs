5be148be3bcb2839e7ea4396b4bc5deef67f9597
Moshe Immerman <moshe@flanksource.com>
2025-07-04 15:01:11 +0300

    chore: wip

---
+++ mission-control/src/pages/about/index.js
@@ -0,0 +1,16 @@
+import Layout from '@theme/Layout';
+import React from 'react';
+
+import About from '../../../../common/src/components/flanksource/About';
+
+function AboutPage() {
+  return (
+    <Layout
+
+    >
+      <About />
+    </Layout>
+  );
+}
+
+export default AboutPage;
