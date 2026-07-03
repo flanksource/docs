b4af35ea32802207409e52f5398c3c45f2f56797
Aditya Thebe <contact@adityathebe.com>
2026-07-03 19:47:33 +0545

    fix(deps): resolve high npm advisories
    Upgrade Docusaurus and webpack, pin serialize-javascript to a patched release, and drop the vulnerable image-webpack-loader/browserify-fs dependency chains. Update the swizzled Docusaurus components for the 3.10 theme internals.

--- common/src/theme/Layout/index.tsx
+++ common/src/theme/Layout/index.tsx
@@ -13,7 +13,6 @@ import {
   SkipToContentFallbackId,
   ThemeClassNames,
 } from '@docusaurus/theme-common';
-import {useKeyboardNavigation} from '@docusaurus/theme-common/internal';
 import SkipToContent from '@theme/SkipToContent';
 import AnnouncementBar from '@theme/AnnouncementBar';
 import Navbar from '@theme/Navbar';
@@ -41,8 +40,6 @@ export default function Layout(props: LayoutProps): ReactNode {
     title,
     description,
   } = props;
-
-  useKeyboardNavigation();
 
   return (
     <LayoutProvider>
